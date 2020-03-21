2c9d2f01977a47d89a7a51eb38ab6bc813ac44dd
admiralbolt <aviknecht@gmail.com>
2020-03-21 17:27:51 -0500

    Dynamically generate breadcrumbs based on current route.

---
+++ client/app/components/unl-breadcrumbs.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  router: service(),
+
+  pageTitle: computed('router.currentRouteName', function() {
+    let routeName = this.get('router.currentRouteName');
+    if (routeName == 'index') return 'Home';
+
+    return `${routeName.charAt(0).toUpperCase()}${routeName.slice(1)}`;
+  })
+});
