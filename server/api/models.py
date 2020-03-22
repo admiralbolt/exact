2fa51a6beb602bb271366a883877b3d1025acad3
admiralbolt <aviknecht@gmail.com>
2020-03-22 15:18:57 -0500

    Added inital server skeleton and models.

---
+++ server/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class EquationType(models.Model):
+  name = models.CharField(max_length=128, unique=True)
+  ordinal = models.FloatField()
+
+
+class Equation(models.Model):
+  name = models.CharField(max_length=128, unique=True)
+  author = models.CharField(max_length=256)
+  date = models.DateField()
+  equation_type = models.ForeignKey(EquationType, on_delete=models.PROTECT)
+  number = models.CharField(max_length=32)
+  # Files!
+  geometry_file = models.FileField(upload_to="geometry/")
+  source_file = models.FileField(upload_to="sources/")
+  content_file = models.FileField(upload_to="content/")
