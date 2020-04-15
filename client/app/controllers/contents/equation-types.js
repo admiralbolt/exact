95bedf4891970b6e9fca7fb9d5bad4e7c88ec1d1
admiralbolt <aviknecht@gmail.com>
2020-04-15 16:24:29 -0500

    Painted the equation types views, fixed issue with improper sorting.

--- client/app/controllers/contents/equation-types.js
+++ client/app/controllers/contents/equation-types.js
@@ -1,9 +1,13 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { sort } from '@ember/object/computed';
 
 export default Controller.extend({
   session: service(),
   creatingNew: false,
+
+  _sortProperties: ['coordinate_system', 'category'],
+  equationTypes: sort('model', '_sortProperties'),
 
   createCallback() {
     this.set('creatingNew', false);
